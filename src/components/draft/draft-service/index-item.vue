3d21bcde8028889082f4960efa5c75ae8a37e42f
shawn <shawn@shawndeMacBook-Pro.local>
2017-02-21 10:49:38 +0800

    [what]:添加文件

---
+++ src/components/draft/draft-service/index-item.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="box" v-for="(indexItems, index) in itemsList" @click="itemClick( index )">
+      <div class="itemImg" >
+        <img :src="indexItems.img" alt=""/>
+      </div>
+      <p>{{ indexItems.title }}--{{ indexItems.img }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['itemClick', 'itemsList']
+  }
+</script>
---
+++ src/components/draft/draft-service/index.vue
@@ -0,0 +1,25 @@
+<template src="./draft-service.html"></template>
+<script>
+  import items from './index-item'
+  export default {
+    components: {
+      items
+    },
+    created () {
+    },
+    data () {
+      document.title = '票据服务'
+      return {
+        itemsList: [{'img': '../../../assets/logo.png', title: 'demo1'},
+                    {'img': '../../../assets/logo.png', title: 'demo1'},
+                    {'img': '../../../assets/logo.png', title: 'demo1'},
+                    {'img': '../../../assets/logo.png', title: 'demo1'}]
+      }
+    },
+    methods: {
+      itemClick (index) {
+        alert('点击>' + index)
+      }
+    }
+  }
+</script>
