7198041a61dd5cb211cccc75ea3ecfb1b8c550d8
shawn <shawn@shawndeMacBook-Pro.local>
2017-02-21 10:45:12 +0800

    [what]:改动

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,18 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 import Hello from 'components/Hello'
+import Item from 'components/draft/draft-service'
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'Hello',
+      component: Item
+    },
+    {
+      path: '/demo/user',
+      name: 'user',
       component: Hello
     }
   ]
